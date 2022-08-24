58a8fe2a3cdc09505682edf62ca5a8f7138a7ebd
Ian Rodrigues <86446951+ianroddrs@users.noreply.github.com>
2022-08-24 20:04:03 -0300

    aula dia 24/08/22
    anotações da aula

---
+++ notes.py
@@ -0,0 +1,48 @@
+'''
+atribuição composta
+a = 1
+a += 2
+a |= b
+b &= a or c
+
+'''
+
+'''
+resposta operadores
+
+1 = D
+2 = C
+3 = A
+4 = D
+5 = B
+
+6 = {
+  A = 1
+  B = 2
+  A, B = B, A
+}
+7 = {
+  Maior = (A + B + abs(A - B)) / 2
+}
+
+8 = {
+  1 - Inteiro
+  2 - String
+  3 - Inteiro
+  4 - Lista (objeto)
+  5 - boolean
+} 
+
+9 = {
+  nome, sobrenome = 'Renato', 'Hidaka'
+}
+
+10 = {
+  X = 10
+  Y = 5
+  X = X ** Y % 3
+  
+  *** NÃO É POSSÍVEL UTILIZAR ATRIBUIÇÃO COMPOSTA POIS IRÁ MUDAR A ORDEM DE PRECEDÊNCIA DAS OPERAÇÕES
+}
+
+'''
